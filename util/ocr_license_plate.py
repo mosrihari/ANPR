d7c355e17dff8988d0bf5deb3fccdaa806a94c6b
Srihari MOHAN <srihari_mohan@nec.com.sg>
2021-05-03 17:02:02 +0800

    tesseract

--- util/ocr_license_plate.py
+++ util/ocr_license_plate.py
@@ -1,5 +1,6 @@
 import pytesseract
 
 def run(image):
+    pytesseract.pytesseract.tesseract_cmd = '/app/.apt/usr/bin/tesseract'
     license_plate_character = pytesseract.image_to_string(image)
     return license_plate_character
